ce24915114d8a3983598d183f1ce4cb4ed1fe1ea
as7haro7 <erickbreack72@gmail.com>
2024-11-03 22:01:41 -0400

    server vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sistema-de-almacenes",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel(),
 });
